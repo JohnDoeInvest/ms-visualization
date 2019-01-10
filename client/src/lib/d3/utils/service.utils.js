77f87b1725d3c80d61de7a3dfb5a8e6286fcdc7a
LamDoan <thanhlam.fit@gmail.com>
2019-01-10 14:43:18 +0700

    Refactor service.utils

--- client/src/lib/d3/utils/service.utils.js
+++ client/src/lib/d3/utils/service.utils.js
@@ -1,42 +1,14 @@
 import { ServiceNode } from '../types/node.types';
 import { ServiceTypes } from '../types/service.types';
+import { getMicroserviceIcon, getRestAPIIcon } from './base.utils';
 
-export const SOURCE_ASSET_BASE_URI = '/assets/d3';
-
-
-function createServiceDescriptionNode(serviceDescriptionArr) {
-    let microservices = [];
-    let restAPIs = [];
-    let topics = [];
-    let sharedServices = [];
-    let stores = [];
-
-    microservices = serviceDescriptionArr.map((service) => ({
-
-    }))
-
-
-    return Object.freeze({
-        microservices,
-        restAPIs,
-        topics,
-        sharedServices,
-        stores
-    });
-}
-
-
-/**
- * 
- * @param {*} microservice 
- * @returns {ServiceNode} serviceNode
- */
 function parseMicroserviceToServiceNode (microservice) {
     const id = microservice.name;
     const name = microservice.name;
     const type = ServiceTypes.Microservice;
     const description = microservice.description;
-    const icon = 
+    const icon = getMicroserviceIcon(microservice);
+
     
     return new ServiceNode({
         id,
@@ -48,6 +20,71 @@ function parseMicroserviceToServiceNode (microservice) {
             isHighlighted: false,
             belongToId: id,
             icon
-        }
+        },
     })
 } 
+
+/**
+ * 
+ * @param {*} restAPI 
+ * {
+        "uri": "/movies",
+        "method": "POST",
+        "parameters": {
+          "ssid": "string with Swedish social security number"
+        }
+    }
+ */
+
+function parseRestAPIToServiceNode (restAPI) {
+    const { uri, method } = restAPI;
+    const id = ServiceTypes.RestAPI.toLowerCase() + '_' + method.toLowerCase() + '_' + uri.toLowerCase();
+    const name = uri;
+    const type = ServiceTypes.RestAPI;
+    const description = microservice.description;
+    const icon = getRestAPIIcon(restAPI);
+    
+    return new ServiceNode({
+        id,
+        name,
+        type,
+        isGroup: false,
+        metadata: {
+            description,
+            isHighlighted: false,
+            belongToId: id,
+            icon
+        },
+    })
+}
+
+/**
+ * {
+        "name": "kafka",
+        "topics": ["clientrequests", "authenticate-bankid"]
+    }
+ * @param {*} topic 
+ */
+
+// function parseTopicToServiceNode (topic) {
+//     const { name,  } = topic;
+//     const id = method.toLowerCase() + '_' + uri.toLowerCase();
+//     const name = uri;
+//     const type = ServiceTypes.RestAPI;
+//     const description = microservice.description;
+//     const icon = getRestAPIIcon(restAPI);
+    
+//     return new ServiceNode({
+//         id,
+//         name,
+//         type,
+//         isGroup: false,
+//         metadata: {
+//             description,
+//             isHighlighted: false,
+//             belongToId: id,
+//             icon
+//         },
+//     })
+// }
+
