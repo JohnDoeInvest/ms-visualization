ad495fa4de5d03259389e0b4217d7c9592b69ac6
LamDoan <thanhlam.fit@gmail.com>
2019-01-13 13:48:15 +0700

    Defined side types of service icons

---
+++ client/src/lib/d3/types/shape.types.js
@@ -0,0 +1,62 @@
+const MicroserviceShapeSideTypes = {
+    ['top']: 'top',
+    ['top-right']: 'top-right',
+    ['top-left']: 'top-left',
+    ['right']: 'right',
+    ['bottom-right']: 'bottom-right',
+    ['bottom']: 'bottom',
+    ['bottom-left']: 'bottom-left',
+    ['left']: 'left'
+};
+
+const RestAPIShapeSideTypes = {
+    ['top']: 'top',
+    ['top-right']: 'top-right',
+    ['top-bottom']: 'top-bottom',
+    ['right']: 'right',
+    ['bottom-top']: 'bottom-top',
+    ['bottom-right']: 'bottom-right',
+    ['bottom']: 'bottom',
+    ['left']: 'left'
+};
+
+const StoreShapeSideTypes = {
+    ['top']: 'top',
+    ['top-right']: 'top-right',
+    ['top-bottom']: 'top-bottom',
+    ['right']: 'right',
+    ['bottom-top']: 'bottom-top',
+    ['bottom-right']: 'bottom-right',
+    ['bottom']: 'bottom',
+    ['left']: 'left'
+};
+
+const TopicShapeSideTypes = {
+    ['top']: 'top',
+    ['top-right']: 'top-right',
+    ['top-bottom']: 'top-bottom',
+    ['right']: 'right',
+    ['bottom-top']: 'bottom-top',
+    ['bottom-right']: 'bottom-right',
+    ['bottom']: 'bottom',
+    ['left']: 'left'
+};
+
+const SharedServiceShapeSideTypes = {
+    ['top']: 'top',
+    ['top-right']: 'top-right',
+    ['top-bottom']: 'top-bottom',
+    ['right']: 'right',
+    ['bottom-top']: 'bottom-top',
+    ['bottom-right']: 'bottom-right',
+    ['bottom']: 'bottom',
+    ['left']: 'left'
+};
+
+export const ShapeSideTypes = {
+    MicroserviceShapeSideTypes,
+    RestAPIShapeSideTypes,
+    StoreShapeSideTypes,
+    TopicShapeSideTypes,
+    SharedServiceShapeSideTypes
+}
