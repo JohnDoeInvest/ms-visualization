4eb6ffca8e83b6a35132cbfe6e233628942ce266
LamDoan <thanhlam.fit@gmail.com>
2019-07-02 09:06:30 +0700

    Fix load service descriptions on github

--- client/src/services/serviceDescription.service.js
+++ client/src/services/serviceDescription.service.js
@@ -4,7 +4,12 @@ const GITHUB_TOKEN = '7d654565e18e027f023514dc0f8fd2e6308edbda';
 const AUTHORIZATION_HEADER = `token ${GITHUB_TOKEN}`;
 
 const createHeaders = ({ token }) => ({
-    'Authorization': `token ${token}`
+    'Authorization': `token ${token}`,
+    // 'Access-Control-Expose-Headers': 'ETag, Link, Retry-After, X-GitHub-OTP, X-RateLimit-Limit, X-RateLimit-Remaining, X-RateLimit-Reset, X-OAuth-Scopes, X-Accepted-OAuth-Scopes, X-Poll-Interval',
+    // 'Access-Control-Max-Age': 86400,
+    // 'Access-Control-Allow-Headers': 'Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, Accept-Encoding, X-GitHub-OTP, X-Requested-With, User-Agent',
+    // 'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE',
+    // 'Access-Control-Allow-Origin': '*'
 })
 
 export async function fetchServiceDescriptionAPI(url) {
@@ -48,10 +53,9 @@ export async function loaddAllCodeContentsAPI({codes, token}) {
         }));
 
         responses = await Promise.all(responses.filter(res => res.ok).map(async (res) => res.json()));
-        const downloadURLs = responses.map(res => res.download_url);
-        let fetchServiceDescriptions = await Promise.all(downloadURLs.map(async (url) => fetchServiceDescriptionAPI(url)));
-
-        return fetchServiceDescriptions;
+        const contents = responses.map(res => res.content);
+        const serviceDescriptions = contents.map(content => JSON.parse(atob(content)));
+        return serviceDescriptions;
     } catch (error) {
         console.error('loaddAllCodeContentsAPI', error);
         throw new Error('Cannot load code contents');
